198cdd5ff23203ac3187359b641dd7f0c45cc662
Beto <albrey02@gmail.com>
2023-01-20 12:25:46 -0600

    primer o segundo commit

---
+++ Encriptador.js
@@ -0,0 +1,17 @@
+console.log("hola")
+
+
+mensaje("Hola mundo");
+
+function separastring(mensaje){
+    let cad = mensaje.split()
+    for(let i=0;i<cad.length;i++){
+        console.log(cad)
+    }
+}
+
+function mensaje(msg){
+
+    return false
+
+}
